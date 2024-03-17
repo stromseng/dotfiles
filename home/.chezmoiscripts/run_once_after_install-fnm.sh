494db9b72b7d8eefec8419b0687f2e44c224b039
Magnus Alexander Strømseng <magnus.stromseng@gmail.com>
2024-03-17 02:05:12 +0100

    check for fnm existance

--- home/.chezmoiscripts/run_once_after_install-fnm.sh
+++ home/.chezmoiscripts/run_once_after_install-fnm.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # Check for fnm command existance
+if command -v fnm &> /dev/null
+then
+    echo "fnm already installed. Skipping..."
+    exit 0
+fi
 
-
-
-curl -fsSL https://fnm.vercel.app/install | bash --skip-shell
+curl -fsSL https://fnm.vercel.app/install | bash -s -- --skip-shell
 
