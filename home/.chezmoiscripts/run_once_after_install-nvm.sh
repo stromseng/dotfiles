25cfafd8ebe7c121781cfaa4705de9b2f58f672e
Magnus Alexander Strømseng <magnus.stromseng@gmail.com>
2024-03-17 02:37:22 +0100

    change node to latest lts version

--- home/.chezmoiscripts/run_once_after_install-nvm.sh
+++ home/.chezmoiscripts/run_once_after_install-nvm.sh
@@ -17,4 +17,4 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
-nvm install node
+nvm install --lts
