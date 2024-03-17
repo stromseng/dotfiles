9f3be6b82c7f6d0d0fe669f873487e4a38c76652
Magnus Alexander Strømseng <magnus.stromseng@gmail.com>
2024-03-17 02:28:31 +0100

    change fnm to nvm

---
+++ home/.chezmoiscripts/run_once_after_install-nvm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Check for fnm command existance
+if command -v nvm &> /dev/null
+then
+    echo "nvm already installed. Skipping..."
+    exit 0
+fi
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
